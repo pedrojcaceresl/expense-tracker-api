a4fa7028178ec78d0d87279ec2fdf90eb0ff00e2
pedro <pedro.jcaceresl@gmail.com>
2025-02-04 20:07:43 -0300

    add HealthCheckController for health check endpoint

---
+++ src/main/java/com/smart/expense_tracker_api/controller/HealthCheckController.java
@@ -0,0 +1,18 @@
+package com.smart.expense_tracker_api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController()
+@RequestMapping("/health")
+public class HealthCheckController {
+    
+    @GetMapping()
+    public ResponseEntity<String> checkHealth() {
+        return ResponseEntity.ok("Everything's up and running...");
+    }
+
+}
