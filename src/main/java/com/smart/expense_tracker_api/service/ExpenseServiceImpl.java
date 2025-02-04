72f2216f21cd846fb667606c8c24f854b1b16451
pedro <pedro.jcaceresl@gmail.com>
2025-02-04 20:08:14 -0300

    implement ExpenseServiceImpl to handle expense operations

---
+++ src/main/java/com/smart/expense_tracker_api/service/ExpenseServiceImpl.java
@@ -0,0 +1,45 @@
+package com.smart.expense_tracker_api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.smart.expense_tracker_api.model.Expense;
+
+/**
+ *
+ * @author Pedro
+ */
+@Service
+public class ExpenseServiceImpl implements ExpenseService {
+
+    @Override
+    public Expense saveExpense(Expense expense) {
+        throw new UnsupportedOperationException("Unimplemented method 'saveExpense'");
+    }
+
+    @Override
+    public List<Expense> getExpensesList() {
+        Expense expense = new Expense();
+        expense.setName("Cinema Hobby");
+        expense.setDescription("Good times");
+        expense.setAmount(25.4);
+
+        List<Expense> expenses = new ArrayList<Expense>();
+        expenses.add(expense);
+
+        return expenses;
+    }
+
+    @Override
+    public Expense updateExpense(Expense expense, Long expenseId) {
+        throw new UnsupportedOperationException("Unimplemented method 'updateExpense'");
+    }
+
+    @Override
+    public void deleteExpenseById(Long expenseId) {
+        throw new UnsupportedOperationException("Unimplemented method 'deleteExpenseById'");
+    }
+
+}
