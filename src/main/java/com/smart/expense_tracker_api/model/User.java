59f674e260b7b9ffd5b7a27900a1265eb9160298
pedro <pedro.jcaceresl@gmail.com>
2025-02-06 17:39:58 -0300

    refactor User model to implement UserDetails, update fields and add timestamps

--- src/main/java/com/smart/expense_tracker_api/model/User.java
+++ src/main/java/com/smart/expense_tracker_api/model/User.java
@@ -1,9 +1,24 @@
 package com.smart.expense_tracker_api.model;
 
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,15 +29,62 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
-    private String name;
-    private String lastname;
+
+    @Column(nullable = false)
+    private String fullName;
+
     private String phoneNumber;
+
+    @Column(unique = true, nullable = false)
     private String email;
     private String username;
     private String password;
-    private String role;
+    
+    @Enumerated(EnumType.STRING)
+    private Role role; // ADMIN, USER
+
+    @Column(nullable = false)
+    private String tenantId;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Expense> expenses = new ArrayList<>();
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
 }
