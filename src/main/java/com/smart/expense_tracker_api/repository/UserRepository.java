58eba45b011025e38a93847e5e31992a5b1907ac
pedro <pedro.jcaceresl@gmail.com>
2025-02-06 17:40:03 -0300

    add method to find User by email in UserRepository

--- src/main/java/com/smart/expense_tracker_api/repository/UserRepository.java
+++ src/main/java/com/smart/expense_tracker_api/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.smart.expense_tracker_api.repository;
 
 import com.smart.expense_tracker_api.model.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
