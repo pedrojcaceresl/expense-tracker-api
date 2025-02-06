0f8346c8696cd99b407d4a4058f5fa8e69829e71
pedro <pedro.jcaceresl@gmail.com>
2025-02-06 17:39:32 -0300

    refactor Expense model to use BigDecimal for amount and establish relationship with User

--- src/main/java/com/smart/expense_tracker_api/model/Expense.java
+++ src/main/java/com/smart/expense_tracker_api/model/Expense.java
@@ -2,12 +2,16 @@
 
 package com.smart.expense_tracker_api.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -25,13 +29,17 @@ import lombok.NoArgsConstructor;
 public class Expense {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long expenseId;
 
     private String name;
     private String category;
-    private double amount;
+    private BigDecimal amount;
     private LocalDate date;
     private String description;
     private String paymentMethod;
+
+    @ManyToOne(cascade =  CascadeType.ALL) // Ensures dependent expenses are deleted
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
