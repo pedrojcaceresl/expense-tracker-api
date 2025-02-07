f1f986f36fdbadfdf3af727b16a1e3442711680e
pedro <pedro.jcaceresl@gmail.com>
2025-02-07 20:35:27 -0300

    add RegisterUserDTO for user registration data transfer

---
+++ src/main/java/com/smart/expense_tracker_api/dto/RegisterUserDTO.java
@@ -0,0 +1,22 @@
+package com.smart.expense_tracker_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Pedro
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegisterUserDTO {
+    private String email;
+
+    private String password;
+
+    private String fullname;
+}
