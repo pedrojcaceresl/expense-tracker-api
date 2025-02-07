83303782de64c4583adf0e24f4cb6bd14590c3cc
pedro <pedro.jcaceresl@gmail.com>
2025-02-07 20:35:36 -0300

    add AuthenticationService for user signup and authentication logic

---
+++ src/main/java/com/smart/expense_tracker_api/service/AuthenticationService.java
@@ -0,0 +1,48 @@
+package com.smart.expense_tracker_api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.smart.expense_tracker_api.dto.LoginUserDTO;
+import com.smart.expense_tracker_api.dto.RegisterUserDTO;
+import com.smart.expense_tracker_api.model.User;
+import com.smart.expense_tracker_api.repository.UserRepository;
+
+/**
+ *
+ * @author Pedro
+ */
+@Service
+public class AuthenticationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public User signup(RegisterUserDTO input) {
+        User user = User.builder()
+                .email(input.getEmail())
+                .fullName(input.getFullname())
+                .password(passwordEncoder.encode(input.getPassword()))
+                .build();
+        userRepository.save(user);
+        return user;
+    }
+
+    public User authenticate(LoginUserDTO input) {
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(input.getEmail(), input.getPassword())
+        );
+
+        return userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+}
