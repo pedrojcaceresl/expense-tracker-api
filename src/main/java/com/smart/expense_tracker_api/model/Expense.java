f6bc6680bcab7880fa31856cacf307f4d1dec9fe
pedro <pedro.jcaceresl@gmail.com>
2025-02-09 12:15:48 -0300

    add validation annotations to Expense model for name and amount fields

--- src/main/java/com/smart/expense_tracker_api/model/Expense.java
+++ src/main/java/com/smart/expense_tracker_api/model/Expense.java
@@ -5,6 +5,8 @@ package com.smart.expense_tracker_api.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -32,8 +37,13 @@ public class Expense {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long expenseId;
 
+    @NotNull
+    @Size(min = 3, max = 100)
     private String name;
     private String category;
+
+    @NotNull
+    @DecimalMin("0.0")
     private BigDecimal amount;
     private LocalDate date;
     private String description;
