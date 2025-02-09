aaa50172a3191b86c2878469b44453e3e4431659
pedro <pedro.jcaceresl@gmail.com>
2025-02-09 12:09:54 -0300

    add createExpense endpoint to ExpenseController with error handling

--- src/main/java/com/smart/expense_tracker_api/controller/ExpenseController.java
+++ src/main/java/com/smart/expense_tracker_api/controller/ExpenseController.java
@@ -14,8 +14,12 @@ import com.smart.expense_tracker_api.service.ExpenseServiceImpl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 /**
@@ -29,9 +33,23 @@ public class ExpenseController {
     @Autowired
     private ExpenseServiceImpl expenseService;
 
+
     @GetMapping()
     public ResponseEntity<List<Expense>> getExpenses() {
         return ResponseEntity.ok(this.expenseService.getExpensesList());
     }
+
+    @PostMapping()
+    public ResponseEntity<Expense> createExpense(@RequestBody Expense expense) {
+        Expense savedExpense = expenseService.saveExpense(expense);
+
+        // Verifica si el objeto se guardó correctamente
+        if (savedExpense.getExpenseId() == 0) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedExpense);
+    }
+    
     
 }
