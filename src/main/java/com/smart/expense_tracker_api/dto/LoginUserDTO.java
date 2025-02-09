3273ffd31d2608ec0438f12f0297f02e65e72318
pedro <pedro.jcaceresl@gmail.com>
2025-02-09 12:10:02 -0300

    rename email field to identifier in LoginUserDTO for improved clarity

--- src/main/java/com/smart/expense_tracker_api/dto/LoginUserDTO.java
+++ src/main/java/com/smart/expense_tracker_api/dto/LoginUserDTO.java
@@ -14,6 +14,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class LoginUserDTO {
-    private String email;
+    private String identifier;
     private String password;
 }
