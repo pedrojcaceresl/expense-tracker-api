f7fff477be837ee259e8273ddcf7a1781a290525
pedro <pedro.jcaceresl@gmail.com>
2025-02-09 12:16:19 -0300

    add findByUsername method and findByEmailOrUsername query to UserRepository

--- src/main/java/com/smart/expense_tracker_api/repository/UserRepository.java
+++ src/main/java/com/smart/expense_tracker_api/repository/UserRepository.java
@@ -5,9 +5,16 @@ import com.smart.expense_tracker_api.model.User;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+
+
+    @Query("SELECT u FROM User u WHERE u.email = :identifier OR u.username = :identifier")
+    Optional<User> findByEmailOrUsername(@Param("identifier") String identifier);
 }
