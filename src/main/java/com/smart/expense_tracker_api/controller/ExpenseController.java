c4cd9317ac127fc1c91b93e31d699f0b3606db50
pedro <pedro.jcaceresl@gmail.com>
2025-02-16 22:24:39 -0300

    add updateExpense and deleteExpense methods to ExpenseController

--- src/main/java/com/smart/expense_tracker_api/controller/ExpenseController.java
+++ src/main/java/com/smart/expense_tracker_api/controller/ExpenseController.java
@@ -11,14 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.smart.expense_tracker_api.model.Expense;
 import com.smart.expense_tracker_api.service.ExpenseServiceImpl;
 
+import jakarta.persistence.EntityNotFoundException;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
@@ -50,6 +56,21 @@ public class ExpenseController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedExpense);
     }
-    
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Expense> updateExpense(@PathVariable Long id, @RequestBody Expense expense) {
+        Expense updatedExpense = expenseService.updateExpense(expense, id);
+        return ResponseEntity.ok().body(updatedExpense);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteExpense(@PathVariable Long id) {
+        try {
+            expenseService.deleteExpenseById(id);
+            return ResponseEntity.ok("Expense with ID " + id + " deleted successfully.");
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
     
 }
