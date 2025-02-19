d631240bbb06171babdad743abcafdcfd3c0456a
pedro <pedro.jcaceresl@gmail.com>
2025-02-18 22:58:48 -0300

    add UserServiceImpl dependency to UserController

--- src/main/java/com/smart/expense_tracker_api/controller/UserController.java
+++ src/main/java/com/smart/expense_tracker_api/controller/UserController.java
@@ -2,13 +2,19 @@ package com.smart.expense_tracker_api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.smart.expense_tracker_api.service.UserServiceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    UserServiceImpl userService;
 
     @GetMapping()
     public String getUsers() {
